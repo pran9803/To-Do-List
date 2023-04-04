70d0a01f5c362c1c54943b934e76a33e9cbddd5e
Pranaya Jayaprakash <ammujay2@gmail.com>
2023-04-04 22:29:13 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [newtask,setnewtask]=useState("");
+  const [task, set_task]=useState([]);  
+  
+  function addTasks(){
+    
+    //console.log(newtask);
+    const tasktobeadded = {
+      id: Math.floor(Math.random()*1000),
+      value: newtask,
+    };
+    set_task((oldlist) => [...oldlist,task]);
+    setnewtask("");
+    console.log(task);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>To Do List</h1>
+       <form>
+        <input className ="inputtingtask" type='text' 
+        placeholder='Add task' 
+        defaultValue={newtask} 
+        onChangeCapture={e => setnewtask(e.target.value)}/>
+      
+        <button onClick={() => addTasks()}>
+          Add
+        </button>
+
+        <ul>
+          {task.map(tasktobeadded =>{
+            return(
+              <li key={tasktobeadded.id}>
+                {tasktobeadded.value}
+
+              </li>
+            )
+          })}
+        
+        </ul>
+        
+       </form >
+
+
+       
+
     </div>
   );
 }
